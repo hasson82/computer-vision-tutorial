7447ff8f6b72ee2103229cbccd66a746c08cde2c
user.name: "Omer Hasson <hasson82@gmail.com>
2024-05-15 20:40:21 +0300

    change file name to indicate which section it describes

--- src/sec2_tutorial.py
+++ src/sec2_tutorial.py
@@ -3,14 +3,12 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 def main():
-    print("Hello World!")
     pic = Image.open("data\\jj_and_omer.jpeg")
-    print(pic)
-    print(type(pic))
     pic_arr = np.asarray(pic)
     plt.imshow(pic_arr)
     pic_red = pic_arr[:,:,0]
     print(pic_red)
+    plt.show()
 
 if __name__ == "__main__":
     main()
