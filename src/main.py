69cd61c0c7bf75cecd407082d871e8d0ce21cee7
user.name: "Omer Hasson <hasson82@gmail.com>
2024-05-15 20:33:20 +0300

    adding basic image showing

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from PIL import Image
 
 def main():
     print("Hello World!")
+    pic = Image.open("data\\jj_and_omer.jpeg")
+    print(pic)
+    print(type(pic))
+    pic_arr = np.asarray(pic)
+    plt.imshow(pic_arr)
+    pic_red = pic_arr[:,:,0]
+    print(pic_red)
 
 if __name__ == "__main__":
     main()
