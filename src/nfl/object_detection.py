bb8cb98aa502adbc556a041b85428e3daf98eb1e
user.name: "Omer Hasson <hasson82@gmail.com>
2024-06-26 23:24:23 +0300

    add video analysis logic

--- src/nfl/object_detection.py
+++ src/nfl/object_detection.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 
-confidence_threshold = 0.55
+confidence_threshold = 0.5
 with open("..\\..\\models\\coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 
@@ -19,6 +19,9 @@ def get_output_layers(net):
     # print(output_layers)
 
     return output_layers
+
+def rotate_image(image):
+    return cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
 def object_detection(image):
     blob = cv2.dnn.blobFromImage(image, scalefactor=0.00392, size=(608, 608), mean=(0, 0, 0), swapRB=True, crop=False)
@@ -42,14 +45,14 @@ def object_detection(image):
                     x, y = int((detection[0] * wT) - w / 2), int((detection[1] * hT) - h / 2)
                     cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
                     cv2.rectangle(blank_image, (x, y), (x+w, y+h), (0, 0, 0), -1)
+    return image
     
-    rotated_image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
-    rotated_blank_image = cv2.rotate(blank_image, cv2.ROTATE_90_COUNTERCLOCKWISE)
-    plt.imshow(rotated_image)
-    plt.show()
+    # rotated_image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    # rotated_blank_image = cv2.rotate(blank_image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    # plt.imshow(rotated_image)
+    # plt.show()
 
-
-def main():
+def image_analysis():
     print("Image Analysis - NFL Defenses")
     nfl_pictures = glob.glob("nfl_coaches_film/*.png")
     
@@ -58,6 +61,52 @@ def main():
         image = cv2.imread(picture)
         rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         object_detection(rgb_image)
+
+def video_analysis():
+    print("Video Analysis - NFL Defenses")
+    video = cv2.VideoCapture("nfl_coaches_film/browns_49ers_offense_1_drive.webm")
+    fps = video.get(cv2.CAP_PROP_FPS)
+    print(fps)
+    delay = int(600/fps)
+    last_frame_hist = None
+    while True:
+        ret, frame = video.read()
+        # If an image frame has been grabbed, display it
+        if ret:
+            frame_resized = cv2.resize(frame, (frame.shape[1] // 2, frame.shape[0] // 2))
+            image_with_detections = frame_resized
+            cv2.imshow('Displaying image frames from video file', image_with_detections)
+            gray = cv2.cvtColor(frame_resized, cv2.COLOR_BGR2GRAY)
+            cur_frame_hist = cv2.calcHist([gray], [0], None, [256], [0, 256])  
+            if last_frame_hist is not None:    
+                cmp = cv2.compareHist(last_frame_hist, cur_frame_hist, cv2.HISTCMP_CORREL)  
+                if cmp < 0.9:
+                    print("Change detected")
+            last_frame_hist = cur_frame_hist
+            # print(f"last frame hist: {last_frame_hist}")
+            # print(f"cur frame hist: {cur_frame_hist}")
+            cv2.waitKey(delay)
+        
+        if not ret:
+            break
+        # rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # object_detection(rgb_frame)
+        if cv2.waitKey(delay) & 0xFF == ord("q"):
+            break
+        # if cv2.waitKey(delay) & 0xFF == ord("d"):
+        #     delay = delay*2
+        #     print("d")
+        # if cv2.waitKey(delay) & 0xFF == ord("a"):
+        #     delay = delay/2
+        #     print("a")
+        # if cv2.waitKey(delay) & 0xFF == ord("s"):
+        #     print("stopped video")
+        #     cv2.waitKey()
+    video.release()
+    cv2.destroyAllWindows()
+
+def main():
+    video_analysis()
 
 if __name__ == "__main__":
     main() 
